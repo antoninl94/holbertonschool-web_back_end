3d1420f80937b0b6329559878a9718de187bde9e
antoninl94 <9803@holbertonstudents.com>
2025-03-24 12:46:46 +0100

    1-block-scoped.js deleted if condition

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
