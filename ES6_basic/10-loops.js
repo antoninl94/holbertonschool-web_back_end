390082f352bfbb7dc263d33f3868b12619398e6e
antoninl94 <9803@holbertonstudents.com>
2025-03-25 13:46:28 +0100

    10-loops.js removed comment eslint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const value = array[idx];
-    // eslint-disable-next-line no-param-reassign
     array[idx] = appendString + value;
   }
 
