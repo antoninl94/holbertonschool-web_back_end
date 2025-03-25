c897729265e3c7c2ff4dd1fdb2f99fcf9c1db137
antoninl94 <9803@holbertonstudents.com>
2025-03-25 14:42:42 +0100

    11-createEmployeesObject.js added $ in loop

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
   const array = [];
   for (const value of employees) {
-    array.push(value);
+    array.push(`'${value}'`);
   }
 
-  return `{${departmentName}: [${array}]}`;
+  return `{ ${departmentName}: [ ${array} ] }`;
 }
