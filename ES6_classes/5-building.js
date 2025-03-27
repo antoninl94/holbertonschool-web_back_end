800725e153692d4737d0556a197a44462fa354be
antoninl94 <9803@holbertonstudents.com>
2025-03-27 19:14:53 +0100

    5-building.js hotfix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
-    }
-
     if (typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-
     this._sqft = sqft;
   }
 
