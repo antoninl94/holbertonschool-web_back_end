e799b1ce9706dc48bd27c314e71538d26a2a73b0
antoninl94 <9803@holbertonstudents.com>
2025-03-27 16:06:13 +0100

    5-building.js changed typeerror to error

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,10 @@
 export default class Building {
   constructor(sqft) {
     if (this.evacuationWarningMessage === undefined) {
-      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
   }
