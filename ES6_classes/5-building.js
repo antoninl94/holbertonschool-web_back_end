766358283dd1312563fb519712ae522cfc9fc2d1
antoninl94 <9803@holbertonstudents.com>
2025-03-27 15:20:34 +0100

    5-building.js created

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,3 +1,19 @@
 export default class Building {
-  
+  constructor(sqft) {
+    if (this.evacuationWarningMessage === undefined) {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
 }
