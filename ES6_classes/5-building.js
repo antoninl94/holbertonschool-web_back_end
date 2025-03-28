4cc4fcb61be0a4dd95dc529a0059288fb8a06efe
antoninl94 <9803@holbertonstudents.com>
2025-03-28 09:55:19 +0100

    5-building.js number check and replaced || by &&

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building || this.evacuationWarningMessage === undefined) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError();
+    }
+    if (this.constructor === Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
