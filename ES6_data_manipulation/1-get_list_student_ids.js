bce3172b108d1dcd3234b2042d7b5cf271a46c85
antoninl94 <9803@holbertonstudents.com>
2025-03-31 14:40:44 +0200

    1-get_list_student_ids.js created

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,22 +2,5 @@ export default function getListStudentIds(studentArray) {
   if (!Array.isArray(studentArray)) {
     return [];
   }
-  studentArray = [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
   return studentArray.map((studentArray) => studentArray.id);
 }
