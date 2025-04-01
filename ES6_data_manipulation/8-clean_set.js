9938207a8203916fc936434d20135c30e8205622
antoninl94 <9803@holbertonstudents.com>
2025-04-01 15:40:28 +0200

    8-clean_set.js added condition for set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,9 @@ export default function cleanSet(set, startString) {
   if (!startString || startString === '') {
     return '';
   }
+  if (!set) {
+    return '';
+  }
   return [...set].filter((element) => element.startsWith('bon'))
     .map((element) => element.slice(startString.length))
     .join('-');
