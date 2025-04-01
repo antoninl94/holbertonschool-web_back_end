047580214f3bed4007bd21c5c5af516d0a983b2a
antoninl94 <9803@holbertonstudents.com>
2025-04-01 09:29:06 +0200

    3-get_ids_sum.js eslint style hotfix

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
 export default function getStudentIdsSum(studentList) {
-  const totalid = studentList.reduce((total, student) => {
-    return total + student.id;
-  }, 0);
+  const totalid = studentList.reduce((total, student) => total + student.id, 0);
   return totalid;
 }
