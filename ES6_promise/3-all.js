144da7ee6ae55b31f65fbd146fdd8d4236495ce5
antoninl94 <9803@holbertonstudents.com>
2025-04-02 14:28:21 +0200

    3-all.js added return to newPromise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   const newPromise = Promise.all([uploadPhoto(), createUser()]);
-  newPromise
+  return newPromise
     .then(([photoBody, userFullName]) => {
       console.log(`${photoBody.body} ${userFullName.firstName} ${userFullName.lastName}`);
     })
