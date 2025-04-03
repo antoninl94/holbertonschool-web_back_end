4bf9ab546428fde1b06cf6a8993354c30daf95c4
antoninl94 <9803@holbertonstudents.com>
2025-04-03 10:14:43 +0200

    9-try.js added Error: to error message

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -7,7 +7,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (e) {
-    queue.push(e);
+    queue.push(`Error: ${e.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
