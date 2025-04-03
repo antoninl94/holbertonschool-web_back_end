7dfb0878f6522d2f2eebd855bdaab7000ac00c20
antoninl94 <9803@holbertonstudents.com>
2025-04-03 09:49:01 +0200

    8-try.js added typeof check for parameters

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -2,6 +2,8 @@ export default function divideFunction(numerator, denominator) {
   try {
     if (denominator === 0) {
       throw new Error('cannot divide by 0');
+    } else if (typeof numerator !== 'number' || typeof denominator !== 'number') {
+      throw new TypeError('Number and denominator must be numbers');
     }
     return numerator / denominator;
   } catch (e) {
