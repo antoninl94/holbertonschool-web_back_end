976c255e3ba4ae85414a4e3ca1b91bede9560d08
antoninl94 <9803@holbertonstudents.com>
2025-04-21 18:07:49 +0200

    all tasks chmod changed

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python3
 """
- type-annotated function make_multiplier that takes a float multiplier
- as argument and returns a function that multiplies a float by multiplier.
+Type-annotated function make_multiplier that takes a float multiplier
+as argument and returns a function that multiplies a float by multiplier.
 """
 from typing import Callable
 
