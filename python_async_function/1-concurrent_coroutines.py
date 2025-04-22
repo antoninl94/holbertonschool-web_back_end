92c477e91fe9aed53542a5084f4c74f4db067185
antoninl94 <9803@holbertonstudents.com>
2025-04-22 11:21:50 +0200

    task 0 and 1 hotfix(pycodestyle)

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,6 +13,7 @@ without using sort() because of concurrency.
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return the list of all the delays"""
     delay_list = []
