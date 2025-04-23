6aa58a35131c041194875b3cff33ada360161ca1
antoninl94 <9803@holbertonstudents.com>
2025-04-23 10:24:40 +0200

    1-concurrent_coroutines.py added create_task and as_completed

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,13 +10,16 @@ wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order
 without using sort() because of concurrency.
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+from asyncio import create_task, as_completed, wait
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return the list of all the delays"""
     delay_list = []
     for _ in range(n):
-        delay = await wait_random(max_delay)
-        delay_list.append(delay)
+        task = create_task(wait_random(max_delay))
+        delay = await task
+        as_completed(delay_list.append(delay))
     return delay_list
