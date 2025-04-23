2d6a560e5105e6b4554ccdc10a0a1cf1485b54fe
antoninl94 <9803@holbertonstudents.com>
2025-04-23 10:38:16 +0200

    3-tasks.py removed async

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,6 +8,6 @@ from asyncio import create_task, Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_random(max_delay: int) -> Task:
+def task_wait_random(max_delay: int) -> Task:
     """returns a asyncio.Task"""
     return create_task(wait_random(max_delay))
