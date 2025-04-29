9914df66cff02318d4b6c3baea9d2810900ec9c2
antoninl94 <9803@holbertonstudents.com>
2025-04-29 09:27:29 +0200

    1-async_comprehension.py modified return type

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,10 +8,10 @@ The coroutine will collect 10 random numbers using an async
 comprehensing over async_generator,
 then return the 10 random numbers.
 """
-from typing import AsyncGenerator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> AsyncGenerator[float, None]:
+async def async_comprehension() -> List[float]:
     """return the result of async_generator"""
     return [num async for num in async_generator()]
