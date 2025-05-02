ebfb9be69bc60d581ab10385fb3a34a01cdf6f4f
antoninl94 <9803@holbertonstudents.com>
2025-05-02 20:20:45 +0200

    11-schools_by_topic.py 10-update_topics created

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -19,5 +19,5 @@ def update_topics(mongo_collection, name, topics):
     """
     school_name = {"name": name}
     update_data = {"$set": {"topics": topics}}
-    result = mongo_collection.update_one(school_name, update_data)
+    result = mongo_collection.update_many(school_name, update_data)
     return result.modified_count
