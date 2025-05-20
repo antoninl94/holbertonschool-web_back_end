60fb38ee916e80f8256207fc35b1b219b30f3b5f
antoninl94 <9803@holbertonstudents.com>
2025-05-20 16:19:08 +0200

    1-stdin.js checked type of input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,6 +6,14 @@ const rl = readline.createInterface({
 });
 
 rl.question("Welcome to Holberton School, what is your name?\n", (answer) => {
+  const name = answer.trim();
+
+  if (!/^[a-zA-Z\s\-']+$/.test(name)) {
+    console.log("Invalid name. Please use only letters, spaces, hyphens or apostrophes.");
+    rl.close();
+    process.exit(1);
+  }
+
   console.log(`Your name is: ${answer}`);
   rl.close();
 
