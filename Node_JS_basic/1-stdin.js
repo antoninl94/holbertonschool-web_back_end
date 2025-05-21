4e33871b21e9ce4edbecc6b7421c47455e9f1c9b
antoninl94 <9803@holbertonstudents.com>
2025-05-21 09:56:16 +0200

    1-stdin.js eslint hotfix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,14 +2,14 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question("Welcome to Holberton School, what is your name?\n", (answer) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   process.stdout.write(`Your name is: ${answer}\n`);
   rl.close();
 
   if (!process.stdin.isTTY) {
-    process.stdout.write("This important software is now closing\n");
+    process.stdout.write('This important software is now closing\n');
   }
 });
