426178cd956cf547b54a23a559878e48ff21ae47
antoninl94 <9803@holbertonstudents.com>
2025-05-21 10:29:13 +0200

    2-read_file.js created

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,15 +1,28 @@
-function countStudents(path) {
-  const fs = require('node:fs');
+const fs = require('node:fs');
 
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const line = data.trim().split("\n");
-    const headers = line[0].split(',');
-    
-    const totalStudents = line.length - 1;
+    const lines = data.trim().split('\n');
+    const headers = lines[0].split(',');
 
-    console.log(data);
-    console.log(`Number of students: ${totalStudents}`)
+    const totalStudents = lines.length - 1;
+    const firstnameIndex = headers.indexOf('firstname');
+    const fieldIndex = headers.indexOf('field');
+
+    const resultCS = lines.slice(1)
+    .map(line => line.split(','))
+    .filter(fields => fields[fieldIndex] === 'CS')
+    .map(fields => fields[firstnameIndex]);
+
+    const resultSWE = lines.slice(1)
+    .map(line => line.split(','))
+    .filter(fields => fields[fieldIndex] === 'SWE')
+    .map(fields => fields[firstnameIndex]);
+
+    process.stdout.write(`Number of students: ${totalStudents}\n`);
+    process.stdout.write(`Number of students in CS: ${resultCS}\n`);
+    process.stdout.write(`Number of students in CS: ${resultSWE}\n`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
