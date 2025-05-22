fddc4301967bf490d44bfa01b8235d4732046078
antoninl94 <9803@holbertonstudents.com>
2025-05-22 09:16:36 +0200

    1-stdin.js eslint hotfix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let name = process.stdin.read();
+  const name = process.stdin.read();
 
   if (name != null) {
     process.stdout.write(`Your name is: ${name}`);
-  };
+  }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write(`This important software is now closing\n`);
+  process.stdout.write('This important software is now closing\n');
 });
